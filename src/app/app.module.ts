3b8770c999c412fe8cb478a74d4070e59fe1c573
Elloumi Abdellaziz <61619978+Ellouminati@users.noreply.github.com>
2022-12-02 18:32:23 +0100

    angular first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome ';
+import { LoginComponent } from './guest/login/login.component';
+import { RegisterComponent } from './guest/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
